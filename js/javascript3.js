f44a79e4cd8610ad05b0cbd8c8256e0a3ceb802f
75841582-netizen <75841582@certus.edu.pe>
2025-11-07 08:36:07 -0500

    Delete js directory

--- js/javascript3.js
+++
@@ -1,7 +0,0 @@
-function demo96(){
-    let total = 100;
-    let _conteo = 3;
-    let $monto2=59.9;
-
-    document.getElementById("out-9-6").textContent = `total = ${total}, _conteo=${_conteo},$monto2=${$monto2}`;
-}
